4661da432b7b80c229870f321f1b7dcbb956e7f9
Mike <michael005@me.com>
2019-10-30 22:42:49 -0400

    First Commit

---
+++ MidtermProject/Minefield.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MidtermProject
+{
+    class Minefield
+    {
+        /// Enum to define the states of each cell in the grid.
+        public enum Cell
+        {
+            EMPTY,
+            M1,
+            M2,
+            M3,
+            M4,
+            M5,
+            M6,
+            M7,
+            M8,
+            M9,
+            MINE
+        };
+    }
+}
